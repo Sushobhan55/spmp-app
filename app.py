42b696583bbcfd73d2ee354ff2c2116aa9aca7e0
Sushobhan Parajuli <sushobhan.parajuli@gmail.com>
2021-12-10 10:23:24 -0500

    app.py splitted to scraper

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import flask
 import pickle
 from text_pipeline import text_pipeline
-from urllib.request import urlopen, Request
-from bs4 import BeautifulSoup
+from scraper import get_news
 import pandas as pd
-import time
 
-todays_date = time.strftime("%b-%d-%y")
 
 app = flask.Flask(__name__, template_folder='templates')
 
@@ -28,30 +25,7 @@ def main():
     if flask.request.method == 'POST':
         ticker = flask.request.form['ticker']
         try:
-            url = 'https://finviz.com/quote.ashx?t=' + ticker.lower()
-            req = Request(url=url, headers={'user-agent': 'my-app'})
-            response = urlopen(req)
-            html = BeautifulSoup(response, features='html.parser')
-            news_table = html.find(id='news-table')
-
-            parsed_news = []
-            for row in news_table.findAll('tr'):
-                text = row.a.text
-                date_data = row.td.text.split(' ')
-                link = row.a["href"]
-
-                if len(date_data) == 1:
-                    time = date_data[0]
-
-                else:
-                    date = date_data[0]
-                    time = date_data[1]
-
-                if date == todays_date:
-                    parsed_news.append([date, time, text, link])
-
-            df = pd.DataFrame(parsed_news)
-            df.columns = ['date', 'time', 'title', 'link']
+            df = get_news(ticker)
 
             predictions = []
             clean_text = []
